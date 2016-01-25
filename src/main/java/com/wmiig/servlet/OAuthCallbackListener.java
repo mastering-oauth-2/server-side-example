8d219a9d6022ea79aa65c7749ec5eaf5ab0e95c4
Charles Bihis <charles@whoischarles.com>
2016-01-24 17:19:18 -0800

    Handle authorization response
    Detect presence of authorization code in response and, if present,
    extract and make access token request.

--- src/main/java/com/wmiig/servlet/OAuthCallbackListener.java
+++ src/main/java/com/wmiig/servlet/OAuthCallbackListener.java
@@ -18,17 +18,61 @@
 
 package com.wmiig.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class OAuthCallbackListener extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-  	throws ServletException, IOException {
-  	// TODO: Detect presence of an authorization code
+    throws ServletException, IOException {
+
+    // Detect the presence of an authorization code
+    String authorizationCode = request.getParameter("code");
+    if (authorizationCode != null && authorizationCode.length() > 0) {
+
+      final String TOKEN_ENDPOINT = "https://graph.facebook.com/oauth/access_token";
+      final String GRANT_TYPE = "authorization_code";
+      final String REDIRECT_URI = "http://wmiig.com/callback";
+      final String CLIENT_ID = "INSERT_CLIENT_ID";
+      final String CLIENT_SECRET = "INSERT_CLIENT_SECRET";
+
+      // Generate POST request
+      HttpPost httpPost = new HttpPost(TOKEN_ENDPOINT
+        + "?grant_type=" + URLEncoder.encode(GRANT_TYPE, StandardCharsets.UTF_8.name())
+        + "&code=" + URLEncoder.encode(authorizationCode, StandardCharsets.UTF_8.name())
+        + "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, StandardCharsets.UTF_8.name())
+        + "&client_id=" + URLEncoder.encode(CLIENT_ID, StandardCharsets.UTF_8.name()));
+
+      // Add "Authorization" header with encoded client credentials
+      String clientCredentials = CLIENT_ID + ":" + CLIENT_SECRET;
+      String encodedClientCredentials = new String(Base64.encodeBase64(clientCredentials.getBytes()));
+      httpPost.setHeader("Authorization", "Basic " + encodedClientCredentials);
+
+      // Make the access token request
+      CloseableHttpClient httpClient = HttpClients.createDefault();
+      HttpResponse httpResponse = httpClient.execute(httpPost);
+
+      // TODO: Handle access token response
+
+      httpClient.close();
+    } else {
+      // Handle failure
+    }
   }
 }
